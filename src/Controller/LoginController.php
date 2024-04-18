7d8d1243055177ce52afc6e43d201f348bb1f5ae
Mohamed Amine El Ouadi <mohamedamineelouadi1@gmail.com>
2024-04-18 08:40:30 +0100

    remove unusable import

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,14 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
 class LoginController extends AbstractController
 {
     
