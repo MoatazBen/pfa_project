b0e7d21e24b43a6fbdb754d828ab3d06b3af8a83
Mohamed Amine El Ouadi <mohamedamineelouadi1@gmail.com>
2024-04-20 17:07:02 +0100

    Product Test

---
+++ tests/ProductTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testName()
+    {
+        $product = new Product();
+        $product->setProductName('Test Product');
+
+        $this->assertEquals('Test Product', $product->getProductName());
+    }
+
+    public function testDescription()
+    {
+        $product = new Product();
+        $product->setDescription('Test Description');
+
+        $this->assertEquals('Test Description', $product->getDescription());
+    }
+
+    public function testPrice()
+    {
+        $product = new Product();
+        $product->setPrice(10.99);
+
+        $this->assertEquals(10.99, $product->getPrice());
+    }
+}
